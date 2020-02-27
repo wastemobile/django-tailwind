90f3a106447aa153461592c85bfb917e80f5e245
wastemobile <wastemobile@users.noreply.github.com>
2020-02-27 16:00:56 +0800

    Update postcss.config.js
    add purgecss run in production env

--- tailwind/app_template/static_src/postcss.config.js
+++ tailwind/app_template/static_src/postcss.config.js
@@ -1,7 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+      '../../templates/**/*.html',
+    ],
+})
+
 module.exports = {
   syntax: 'postcss-scss',
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
